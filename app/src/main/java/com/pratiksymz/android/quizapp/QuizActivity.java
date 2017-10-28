96de1e7a6f4bbc8a997497b06b7fb6e5630878e3
pratiksymz <pratikbudhiraja@gmail.com>
2017-10-28 16:00:24 +0530

    Changed the Package Name

--- app/src/main/java/com/pratiksymz/android/quizapp/QuizActivity.java
+++ app/src/main/java/com/pratiksymz/android/quizapp/QuizActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.quizapp;
+package com.pratiksymz.android.quizapp;
 
 import android.content.Intent;
 import android.os.Bundle;
